8a7af6678cdb16493ea68489adfd367b16ed4f7d
KurKing <lyosha.kurkin@gmail.com>
2021-12-10 21:18:39 +0200

    c# lab knapsack

---
+++ Algoritms/Knapsack/Knapsack/Knapsack/Knapsack.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Knapsack
+{
+    public class Knapsack
+    {
+        private readonly List<Item> _items;
+        public int Weight => _items.Sum(item => item.Weight);
+        public int Value => _items.Sum(item => item.Value);
+
+        public Knapsack(List<Item> items)
+        {
+            _items = items;
+        }
+
+        public List<Item> Items => _items;
+    }
+}

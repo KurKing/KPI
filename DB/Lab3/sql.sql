d635846b0c578f138003ab0e118cec388edfb046
KurKing <lyosha.kurkin@gmail.com>
2021-12-16 23:02:37 +0200

    db lab 3 part 1

---
+++ DB/Lab3/sql.sql
@@ -0,0 +1,106 @@
+-- #1
+
+-- 1
+SELECT name, price
+FROM products
+WHERE name = 'Apple';
+-- 2
+SELECT name, price
+FROM products
+WHERE name LIKE '%5%';
+-- 3
+SELECT sum(price)
+FROM products
+WHERE name LIKE '%pple%';
+-- 4
+SELECT shipping_company, price
+FROM shipping_methods
+WHERE is_express;
+-- 5
+SELECT name, price
+FROM products
+WHERE  price >= 1000;
+-- 6
+SELECT name, price
+FROM products
+WHERE price > 500 AND price < 2000;
+-- 7
+SELECT name
+FROM products
+WHERE price IS NULL;
+-- 8
+SELECT name, price
+FROM products
+WHERE price BETWEEN 200 AND 600;
+-- 9
+SELECT avg(price)
+FROM products
+WHERE name = 'Apple' OR name = 'Banana' OR name IS NULL;
+-- 10
+SELECT name, price
+FROM products;
+-- 11
+SELECT date, id
+FROM orders
+WHERE date < current_timestamp;
+-- 12
+SELECT name
+FROM products
+WHERE price = 300;
+-- 13
+SELECT name, producer
+FROM products
+WHERE producer LIKE '%company%';
+-- 14
+SELECT name, producer
+FROM products
+WHERE producer NOT LIKE '%company%';
+-- 15
+SELECT id
+FROM orders
+WHERE extract(MONTH FROM date) = 12;
+
+-- #2
+
+-- 1
+SELECT name
+FROM products
+JOIN contract_cases ON products.id = contract_cases.product_id
+WHERE contract_id IN (
+    SELECT id
+    FROM contracts
+    WHERE contracts.total_price > 10000
+);
+-- 2
+SELECT products.name, products.price
+FROM contract_cases
+RIGHT JOIN products on contract_cases.product_id = products.id
+WHERE contract_cases IS NULL;
+-- 3
+SELECT products.name, products.price
+FROM products
+LEFT JOIN contract_cases on contract_cases.product_id = products.id
+WHERE contract_cases IS NULL;
+-- 4
+
+-- 5
+
+-- 6
+
+-- 7
+
+-- 8
+
+-- 9
+
+-- 10
+
+-- 11
+
+-- 12
+
+-- 13
+
+-- 14
+
+-- 15

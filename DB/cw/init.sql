2503bab9da6f241f3e0a638022873126f60c2e0c
KurKing <lyosha.kurkin@gmail.com>
2021-12-21 10:59:12 +0200

    `DB` `CW` multi user

--- DB/cw/init.sql
+++ DB/cw/init.sql
@@ -4,3 +4,8 @@ CREATE DATABASE "restaurant-main-db" owner restaurant_owner;
 ALTER DATABASE "restaurant-main-db" SET TIMEZONE = 'UTC';
 
 GRANT ALL PRIVILEGES ON DATABASE "restaurant-main-db" TO restaurant_owner;
+
+CREATE USER restaurant_reader WITH PASSWORD 'restaurant_reader';
+GRANT CONNECT ON DATABASE "restaurant-main-db" TO restaurant_reader;
+GRANT USAGE ON SCHEMA public TO restaurant_reader;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO restaurant_reader;

23b86b93645c15d3c6f50b84c0a5db9e02d14436
KurKing <lyosha.kurkin@gmail.com>
2022-01-07 16:45:54 +0200

    [pa-lab6] assets

---
+++ Algoritms/TabooGame/TabooGame/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  TabooGame
+//
+//  Created by Oleksiy on 07.01.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var fruitSalad: UIColor {
+        0x42944b.hexColor
+    }
+}
